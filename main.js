73db551236ea3e55b2203313899ede007441de95
Joe Hull <josephghull@gmail.com>
2018-05-01 10:08:03 -0400

    Add index and main script

---
+++ main.js
@@ -0,0 +1,9 @@
+ReactDOM.render(
+    React.createElement('a', { href: "https://en.wikipedia.org/wiki/Border_Collie" },
+      React.createElement('img', {
+        src: "http://cdn1-www.dogtime.com/assets/uploads/2011/01/file_23178_border-collie-460x290.jpg"
+      })
+    ),
+     
+    document.getElementById('app')
+);
